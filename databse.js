74305544f7f023801e6a9e134b2014234194353b
zarghamkhandev <zarghamkhan980@gmail.com>
2020-09-11 23:06:26 +0200

    heorku

--- databse.js
+++ databse.js
@@ -1,7 +1,6 @@
 const Sequelize = require("sequelize");
 
 
-module.exports =  new Sequelize("codegig","postgres","password",{
-    host: "localhost",
+module.exports =  new Sequelize(process.env.DATABASE_URL,{
     dialect: "postgres",
 })
